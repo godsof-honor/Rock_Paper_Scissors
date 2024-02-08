d40698107b169ce2c33ca9e5fda5fc575d4ee9ca
Enrique Vargas Jr. <ejvargas247@gmail.com>
2024-02-07 18:52:22 -0700

    Created computer function

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("Hello, World")
+let computerAnswer;
+
+function getComputerChoice(){
+    
+    let intChoice = getRandomInt(3);
+    
+    let computerAnswer;
+    if (intChoice === 0){
+        computerAnswer = "Rock";
+    } else if (intChoice === 1){
+        computerAnswer = "Paper";
+    } else {
+        computerAnswer = "Scissors";
+    }
+    console.log(computerAnswer);
+    return computerAnswer;
+}
+
+function getRandomInt(max){
+    return Math.floor(Math.random() *max);
+}
+
+let playerSelection;
+let computerSelection;
+
+
+computerSelection = getComputerChoice();
+
+
+// function playround(playerSelection, computerSelection){
+
+// }
+
